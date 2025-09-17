15c4622b948f9f02d7aac17d0709df0a73c4a05e
Avinashkumar Kadiyam <avinashrk23.pumba@gmail.com>
2025-09-17 07:10:44 +0000

    npm run deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  // basePath: '/your-repo-name', // Update this with your repository name
   typescript: {
     ignoreBuildErrors: true,
   },
